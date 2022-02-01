8afa7556da91db397f5b447059b22ad88695c58c
jhenrique09 <jhsv09@gmail.com>
2022-02-01 20:55:48 +0530

    vendor: Add QuickTap sysconfig
    Change-Id: I3c308e5917c574b109820dd3ceb396f923174751
    Signed-off-by: Pratyaksh Bharadwak <pratyakshom.05@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -132,6 +132,12 @@ PRODUCT_PACKAGES += \
     product_charger_res_images
 endif
 
+# Quick Tap
+ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
+PRODUCT_COPY_FILES += \
+    vendor/octavi/prebuilt/common/etc/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+endif
+
 # Storage manager
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.storage_manager.enabled=true
