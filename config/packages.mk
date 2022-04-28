c51a79e5bdbbe02900fc5046eeec4bddff7e2277
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2022-04-28 12:21:02 +0000

    config: drop custom Signal/Wifi icons
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -8,28 +8,6 @@ PRODUCT_PACKAGES += \
     Updater \
     ThemePicker
 
-# Statusbar Icons
-PRODUCT_PACKAGES += \
-    StrokeSignalOverlay \
-    SneakySignalOverlay \
-    XperiaSignalOverlay \
-    ZigZagSignalOverlay \
-    WavySignalOverlay \
-    RoundSignalOverlay \
-    InsideSignalOverlay \
-    BarsSignalOverlay
-
-# Wi-Fi Icons
-PRODUCT_PACKAGES += \
-    StrokeWiFiOverlay \
-    SneakyWiFiOverlay \
-    XperiaWiFiOverlay \
-    ZigZagWiFiOverlay \
-    WavyWiFiOverlay \
-    RoundWiFiOverlay \
-    InsideWiFiOverlay \
-    BarsWiFiOverlay
-
 # Extra tools in Awaken
 PRODUCT_PACKAGES += \
     7z \
